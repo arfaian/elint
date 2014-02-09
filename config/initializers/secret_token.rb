c8fd7f17668d3d2e560b4f0d45248bd68a684497
Arian Arfaian <arian.arfaian@gmail.com>
2014-02-09 14:09:57 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Elint::Application.config.secret_key_base = '009e1744d9cf8118451b6a360c347ed70c4fbabda55618857de5f8175210eb22bc0f304b83bbfc9c452fe781cf8cb4434603867a70b52a832ef85c98387d2d7a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Elint::Application.config.session_store :cookie_store, key: '_elint_session'
